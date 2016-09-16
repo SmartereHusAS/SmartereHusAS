ac74fd04efcaebde00fb08c5ae45e7bd6082dc80
evendalen <even.dalen@live.no>
2016-09-16 13:47:50 +0200

    Ferdig med room og house

---
+++ src/main/java/org/chiaboy/webapp/House.java
@@ -0,0 +1,33 @@
+package org.chiaboy.webapp;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * Created by Evdal on 16.09.2016.
+ */
+@Path("/room")
+class House{
+    private static HashMap<String,Room> rooms = new HashMap<String,Room>();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Room> getRooms(){
+        return rooms.values();
+    }
+    @GET
+    @Path("/{roomId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Room getRoom(@PathParam("roomId") String roomId){
+        return rooms.get(roomId);
+    }
+    @POST
+    @Path("/{roomId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addRoom(Room room){
+        rooms.put(room.getId(),room);
+    }
+
+}
---
+++ src/main/java/org/chiaboy/webapp/Room.java
@@ -0,0 +1,47 @@
+package org.chiaboy.webapp;
+
+/**
+ * Created by Evdal on 16.09.2016.
+ */
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/room")
+class Room{
+    private Sounds sounds;
+    private Lights lights;
+    private Temperatures temps;
+    private String name;
+    private String id;
+
+    public Room(Sounds sounds, Lights lights, Temperatures temps, String name, String id){
+        this.sounds = sounds;
+        this.lights = lights;
+        this.temps = temps;
+        this.name = name;
+
+    }
+    public String getId(){
+        return id;
+    }
+
+    @GET
+    @Path("/sounds")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Sounds getSounds() {
+        return sounds;
+    }
+    @GET
+    @Path("/lights")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Lights getLights() {
+        return lights;
+    }
+    @GET
+    @Path("/temps")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Temperatures getTemps() {
+        return temps;
+    }
+
+}
